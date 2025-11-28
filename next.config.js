bb7327918aa925a3b73c366f2123c6b357a2e77e
Geralt Ji <qq547608233@gmail.com>
2025-11-28 15:11:47 +0800

    css URL

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'summer_winter_camp'
+
 const nextConfig = {
   output: 'export',
+  basePath: isProd ? `/${repo}` : undefined,
+  assetPrefix: isProd ? `/${repo}` : undefined,
+  images: { unoptimized: true },
 }
 
 module.exports = nextConfig
